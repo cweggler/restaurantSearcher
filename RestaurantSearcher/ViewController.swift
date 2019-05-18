0296f81daa1384515331b899828f7d1dcedc0725
Cara Weggler <cweggler@gmail.com>
2019-05-17 20:06:10 -0500

    Initial Commit

---
+++ RestaurantSearcher/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RestaurantSearcher
+//
+//  Created by Cara on 5/17/19.
+//  Copyright © 2019 Cara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
